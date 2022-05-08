3259a9b82e11960b47a777a9f9ac540e2f0577c3
LoloziAnas <lolozianas1@gmail.com>
2022-05-07 22:29:49 -0700

    adding spring security - implementing jwt

--- src/main/java/com/morobyte/ebankingbackend/sec/config/SecurityConfig.java
+++ src/main/java/com/morobyte/ebankingbackend/sec/config/SecurityConfig.java
@@ -1,20 +1,43 @@
 package com.morobyte.ebankingbackend.sec.config;
 
+import com.morobyte.ebankingbackend.sec.entity.AppUser;
+import com.morobyte.ebankingbackend.sec.filter.JwtAuthenticationFilter;
+import com.morobyte.ebankingbackend.sec.service.IAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private IAccountService accountService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().disable();
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("h2-console/**").permitAll();
+        //http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(new JwtAuthenticationFilter(authenticationManagerBean()));
     }
 
     @Override
@@ -24,6 +47,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                AppUser appUser = accountService.getUserByUsername(username);
+                Collection<GrantedAuthority> authorities = new ArrayList<>();
+                appUser.getAppRoles().forEach(role -> {
+                    authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+                });
+                return new User(appUser.getUsername(), appUser.getPassword(), authorities);
+            }
+        });
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
