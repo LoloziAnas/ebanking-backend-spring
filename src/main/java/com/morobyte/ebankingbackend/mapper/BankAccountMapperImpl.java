64531d7a7e9ea34bfde63d6ca72f5c9185652163
LoloziAnas <lolozianas1@gmail.com>
2022-05-14 23:08:41 -0700

    Exception handling - implement DTOs

---
+++ src/main/java/com/morobyte/ebankingbackend/mapper/BankAccountMapperImpl.java
@@ -0,0 +1,15 @@
+package com.morobyte.ebankingbackend.mapper;
+
+import com.morobyte.ebankingbackend.dto.CustomerDto;
+import com.morobyte.ebankingbackend.entity.Customer;
+
+public class BankAccountMapperImpl {
+
+    public static CustomerDTO toCustomerDTO(Customer customer) {
+        return null;
+    }
+
+    public static Customer toCustomerEntity(CustomerDto customerDto) {
+        return null;
+    }
+}
