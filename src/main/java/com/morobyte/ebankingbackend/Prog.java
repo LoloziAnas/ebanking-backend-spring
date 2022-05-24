b7c604425ad24ff5cbb62da8ae616bfc68a855be
LoloziAnas <lolozianas1@gmail.com>
2022-05-23 19:39:09 -0700

    Exception handling - implement DTOs

---
+++ src/main/java/com/morobyte/ebankingbackend/Prog.java
@@ -0,0 +1,11 @@
+package com.morobyte.ebankingbackend;
+
+public class Prog {
+
+    public static void main(String[] args) {
+        String[] colors = {"Red", "Yellow", "Blue", "Green", "White"};
+        for (String s : colors) {
+            System.out.println(s);
+        }
+    }
+}
