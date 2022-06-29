0c96dffe6181074a30d99d8ed548249281e852fe
LoloziAnas <lolozianas1@gmail.com>
2022-06-28 22:51:10 -0700

    Exception handling - implement DTOs

--- src/main/java/com/morobyte/ebankingbackend/exception/ResourceNotFoundException.java
+++ src/main/java/com/morobyte/ebankingbackend/exception/ResourceNotFoundException.java
@@ -3,5 +3,6 @@ package com.morobyte.ebankingbackend.exception;
 public class ResourceNotFoundException extends Exception{
     public ResourceNotFoundException(String message){
         super(message);
+
     }
 }
