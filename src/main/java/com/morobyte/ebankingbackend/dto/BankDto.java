19a3b3835a19afc048429f47e2087a04f4c5f644
LoloziAnas <lolozianas1@gmail.com>
2022-07-22 21:10:53 -0700

    Exception handling - implement DTOs

--- src/main/java/com/morobyte/ebankingbackend/dto/BankDto.java
+++ src/main/java/com/morobyte/ebankingbackend/dto/BankDto.java
@@ -3,9 +3,11 @@ package com.morobyte.ebankingbackend.dto;
 public class BankDto {
     private final Long id;
     private final String name;
+    private final String desc;
 
     public BankDto(Long id, String name) {
         this.id = id;
         this.name = name;
     }
+
 }
